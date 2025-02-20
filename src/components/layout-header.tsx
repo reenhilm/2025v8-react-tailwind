90389947aa7f1c722ca9deaa3ca367965c11bc5c
Christian Rönnholm <christian@kajal.se>
2025-02-20 17:05:18 +0100

    changed pos of header in grid

--- src/components/layout-header.tsx
+++ src/components/layout-header.tsx
@@ -4,14 +4,12 @@ import { ModeToggle } from "./mode-toggle";
 export default function LayoutHeader() {
     return (
         <>
-            <nav className="col-start-1 -col-end-2 flex justify-end items-center h-5 min-h-10">
+            <h1 className="text-3xl col-start-1 col-end-3">ReceiptDB</h1>
+            <nav className="col-start-3 -col-end-2 flex justify-end items-center h-5 min-h-10">
                 <Link href="/" className="mx-1 py-1 px-3 font-semibold uppercase">Home</Link>
                 <Link href="/receipts" className="mx-1 py-1 px-3 font-semibold uppercase">Receipts</Link>
-            </nav>
-            <div className="-col-start-2 -col-end-1 min-h-10 flex items-center">
                 <ModeToggle />
-            </div>            
-            <h1 className="text-3xl">ReceiptDB</h1>
+            </nav>            
         </>
     );
 }
